32bd91391297815ae8c76d4dba419cc5240b6c6a
Carlos Ortega <coforfe@users.noreply.github.com>
2021-05-24 20:24:45 +0200

    To reproduce logo Madrid R sticker.
    To create the "Madrid R Group" hex sticker.

---
+++ logoMadridR_sticker_.R
@@ -0,0 +1,9 @@
+library(hexSticker)
+imgurl <- "./madridr_300v2.png"
+sticker(imgurl, package="Grupo R Madrid", 
+        p_size = 6, p_y = 1.25, p_color = 'black',
+        s_x = 1, s_y = 0.75,  s_width = 0.73, s_height = 0.73,
+        h_size = 0.7, h_fill = "white", h_color = "turquoise3", 
+        url = "madrid.r-es.org/", u_size = 1, u_color = "black",
+        filename = "./HexGrupoRMadridturquesa.png")
+
